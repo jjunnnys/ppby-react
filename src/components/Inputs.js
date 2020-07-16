7ad4f725c7cd9ee6898bfbebd171d3aded10315c
ppby <salswnss@gmail.com>
2020-07-16 11:30:23 +0900

    Add styled button

--- src/components/Inputs.js
+++ src/components/Inputs.js
@@ -1,4 +1,28 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const Button = styled.button`
+  margin: 1rem 0;
+  width: 5rem;
+  height: 3rem;
+  border: none;
+  background-color: #bac8ff;
+  border-radius: 5px;
+
+  font-size: 2rem;
+  font-weight: 700;
+
+  cursor: pointer;
+  transition: background-color 0.3s ease-in;
+
+  &:focus {
+    outline: none;
+  }
+
+  &:hover {
+    background-color: #4c6ef5;
+  }
+`;
 
 const Inputs = ({ onChange }) => {
   return (
@@ -15,6 +39,7 @@ const Inputs = ({ onChange }) => {
         placeholder="이메일을 입력하세요."
         onChange={onChange}
       />
+      <Button>추가</Button>
     </>
   );
 };
